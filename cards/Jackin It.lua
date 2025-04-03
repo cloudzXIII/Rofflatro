f91abbdf2dda7a6cc7af6240da3d31d2a238a124
AlrexX <alexsopena789@gmail.com>
2025-04-04 00:11:33 +0200

    loc files, jackin it

---
+++ cards/Jackin It.lua
@@ -0,0 +1,28 @@
+SMODS.Joker{
+    key = 'jackin_it',
+    atlas = 'roffers',
+    pos = {x = 1, y = 0},
+    rarity = 1, 
+	blueprint_compat = true,
+    perishable_compat = false,
+    cost = 4,
+    config = { extra = {mult = 0, mult_mod = 3} },
+    loc_vars = function (self, info_queue, card)
+        return { vars = {card.ability.extra.mult_mod, card.ability.extra.mult} }
+    end,
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and not context.blueprint then
+            if context.other_card:get_id() == 11 then
+                card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_mod
+                return {
+                    message = localize('k_upgrade_ex'),
+                    card = card
+                }
+            end
+        elseif context.joker_main then
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+    end
+}
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,16 +1,26 @@
 return {
     descriptions = {
         Joker = {
-            roff_typeA = {
+            j_roff_typeA = {
                 name = 'Type A Joker',
                 text = {
                     'Gains {X:mult,C:white}X#1#{} Mult if played',
                     'hand is a {C:attention}#3#{}.',
                     'Resets when playing a {C:attention}High Card{}.',
                     'Hand changes at end of round.',
-                    '{C:inactive}(Currently {}{X:mult,C:white}X#2#{}{C:inactive} mult){}',
+                    '{C:inactive}(Currently {}{X:mult,C:white}X#2#{}{C:inactive} Mult){}',
+                    '{s:0.8,C:inactive}Streamer! Play the #3#!{} {s:0.8,C:inactive,E:1}STREAMER!!!{}'
                 }
             },
+            j_roff_jackin_it = {
+                name = 'Jackin\' it',
+                text = {
+                    'This Joker gains {C:mult}+#1#{} Mult',
+                    'for every scored {C:attention}Jack{}',
+                    '{C:inactive}(Currently {}{C:mult}+#2#{} {C:inactive}Mult){}',
+                    '{s:0.8,C:inactive}On stream? How inappropiate!'
+                }
+            }
         }
     }
 }
