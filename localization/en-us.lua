e952f171489495ef33240a3e5166a0fe2e31ece7
AlrexX <alexsopena789@gmail.com>
2025-04-03 23:44:12 +0200

    Update en-us.lua

---
+++ localization/en-us.lua
@@ -0,0 +1,16 @@
+return {
+    descriptions = {
+        Joker = {
+            roff_typeA = {
+                name = 'Type A Joker',
+                text = {
+                    'Gains {X:mult,C:white}X#1#{} Mult if played',
+                    'hand is a {C:attention}#3#{}.',
+                    'Resets when playing a {C:attention}High Card{}.',
+                    'Hand changes at end of round.',
+                    '{C:inactive}(Currently {}{X:mult,C:white}X#2#{}{C:inactive} mult){}',
+                }
+            },
+        }
+    }
+}
