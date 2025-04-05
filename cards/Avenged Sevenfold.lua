1d5646379c258a8e169531c12ac4706f66456032
AlrexX <alexsopena789@gmail.com>
2025-04-06 00:09:31 +0200

    a7x badges + upgrade to uncommon

--- cards/Avenged Sevenfold.lua
+++ cards/Avenged Sevenfold.lua
@@ -1,7 +1,8 @@
 SMODS.Joker{
 	key = 'a7x',
 	atlas = 'roffers',
-	rarity = 1,
+	rarity = 2,
+    cost = 6,
 	blueprint_compat = true,
 	perishable_compat = false,
 	pos = { x = 5, y = 1 },
@@ -38,5 +39,9 @@ SMODS.Joker{
                 mult = card.ability.extra.mult
             }
         end
+	end,
+	set_badges = function (self, card, badges)
+		badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
+		badges[#badges+1] = create_badge(localize('k_roff_credit_l6_code'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
 	end
 }
