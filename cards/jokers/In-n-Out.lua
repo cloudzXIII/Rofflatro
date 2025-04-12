28f1ff5e2c0fe85abf91664c00011696b6bf6cab
threeofaband <sycisytation@gmail.com>
2025-04-12 09:29:26 +0800

    Update In-n-Out.lua

--- cards/jokers/In-n-Out.lua
+++ cards/jokers/In-n-Out.lua
@@ -18,7 +18,7 @@ SMODS.Joker{
    cost = 5,
    loc_vars = function(self, info_queue, card)
       return {
-         vars = { card.ability.extra.chips }
+         vars = { card.ability.extra.chipgain, card.ability.extra.chips }
       }
    end,
    calculate = function(self, card, context)
