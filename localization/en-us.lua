28ac96a00844e949fb0dbe10d0d1f6c833781edb
threeofaband <sycisytation@gmail.com>
2025-04-12 09:30:45 +0800

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -197,6 +197,7 @@ return {
             k_roff_gross_dead = 'Good Riddance!',
             k_roff_gross_add_banana = 'Gross!',
             k_roff_pivot_active = 'Pivot!',
+            k_roff_inout_fail = 'Bad taste!'
 
             k_roff_credit_l6_art = 'Art: Lucky6/AlrexX',
             k_roff_credit_l6_code = 'Code: Lucky6/AlrexX',
