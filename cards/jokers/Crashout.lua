562c1f8101cd1acdc249f0de179a17283cd86206
Food <80005511+UHadMeAtFood@users.noreply.github.com>
2025-04-25 22:08:38 -0400

    Add files via upload

---
+++ cards/jokers/Crashout.lua
@@ -0,0 +1,61 @@
+-- Crashout (Rare) - When blind is selected, destroys all owned consumables. Gains x0.25 Mult per card destroyed. (Why is streamer so mad?)
+
+
+
+SMODS.Joker{
+	key = 'crashout',
+	loc_txt = {
+		name = 'crashout',
+		text = {
+			'When blind is selected', 
+			'destroys all owned consumables.',
+			' Gains {X:mult,C:white}X#2#{} Mult per card destroyed.',
+			'{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)',
+			'{C:inactive,s:0.8,E:2} Why is streamer so mad?{C:inactive,s:0.8,E:1}'
+		}
+	},
+	atlas = 'roffers',
+	rarity = 1,
+	cost = 1,
+	blueprint_compat = true,
+	eternal_compat = true,
+	pos = { x = 0, y = 0 },
+	config = { extra = {
+			Xmult = 1,
+			Xmult_mod = .25
+		}
+	},
+	loc_vars = function(self,info_queue,card)
+		return {vars = {card.ability.extra.Xmult, card.ability.extra.Xmult_mod}}
+	end,
+	calculate = function(self,card,context)
+		if context.setting_blind and not context.blueprint then
+			card.ability.extra.Xmult = card.ability.extra.Xmult + (#G.consumeables.cards* card.ability.extra.Xmult_mod)
+			for Ncard = 1, #G.consumeables.cards do 
+				local _card = G.consumeables.cards[Ncard]
+				G.E_MANAGER:add_event(Event({
+					trigger = 'after',
+					delay = 0.3,
+					blockable = false,
+					func = function()
+						_card:start_dissolve({HEX("deb1b1")}, nil, 1.6)
+						_card = nil
+						return true;
+					end
+				}))
+			end
+			return{
+				message = "Crashout",
+				colour = HEX("deb1b1")
+			}
+		end
+
+		if context.joker_main then
+			return{
+				Xmult = card.ability.extra.Xmult
+			}
+		end
+
+
+	end
+}
