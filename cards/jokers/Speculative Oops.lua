8010c1a9553685c673e4aa0c5b97fe4a91b52e99
threeofaband <sycisytation@gmail.com>
2025-05-25 16:40:01 +0800

    Update Speculative Oops.lua
    cursed

--- cards/jokers/Speculative Oops.lua
+++ cards/jokers/Speculative Oops.lua
@@ -6,13 +6,18 @@ SMODS.Joker{
 	blueprint_compat = true,
 	eternal_compat = true,
 	pos = { x = 1, y = 6 },
-	config = { extra = { dollars = 10, odds = 2 } },
+	config = { extra = { dollars = 16, odds = 2 } },
 	loc_vars = function(self,info_queue,card)
 		return {vars = {G.GAME.probabilities.normal^4 or 1, card.ability.extra.odds, card.ability.extra.dollars}}
 	end,
 	calc_dollar_bonus = function(self, card)
 		if pseudorandom('roff_spec_oops') <= (G.GAME.probabilities.normal)^4 / (card.ability.extra.odds)^4 then
 			return card.ability.extra.dollars
+		else
+			SMODS.calculate_context({roff_probability_missed = true}, effects)
+			SMODS.calculate_context({roff_probability_missed = true}, effects)
+			SMODS.calculate_context({roff_probability_missed = true}, effects)
+			SMODS.calculate_context({roff_probability_missed = true}, effects)
 		end
 	end,
 	set_badges = function (self, card, badges)
