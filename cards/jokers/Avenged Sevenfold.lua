83b08c982fedab59ba5f0e84acd2a24e9fb5b0c9
threeofaband <sycisytation@gmail.com>
2025-05-25 16:09:14 +0800

    Update Avenged Sevenfold.lua
    Code Cleanup
    - why for loop twice??

--- cards/jokers/Avenged Sevenfold.lua
+++ cards/jokers/Avenged Sevenfold.lua
@@ -1,33 +1,27 @@
 SMODS.Joker{
-	key = 'a7x',
-	atlas = 'roffers',
-	rarity = 2,
+    key = 'a7x',
+    atlas = 'roffers',
+    rarity = 2,
     cost = 6,
-	blueprint_compat = true,
-	perishable_compat = false,
-	pos = { x = 2, y = 3 },
-	config = { extra = { mult = 0, mult_mod = 4 } },
-	loc_vars = function(self,info_queue,card)
-		return {vars = {card.ability.extra.mult_mod, card.ability.extra.mult}}
-	end,
-	calculate = function(self,card,context)
-		if context.before and not context.blueprint then
+    blueprint_compat = true,
+    perishable_compat = false,
+    pos = { x = 2, y = 3 },
+    config = { extra = { mult = 0, mult_mod = 4 } },
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.mult_mod, card.ability.extra.mult}}
+    end,
+    calculate = function(self,card,context)
+        if context.before and not context.blueprint then
             local hasA, has7 = false, false
-
             for _, c in pairs(context.scoring_hand) do
                 if c:get_id() == 7 then
                     has7 = true
                     break
-                end
-            end
-
-            for _, c in pairs(context.scoring_hand) do
-                if c:get_id() == 14 then
+                elseif c:get_id() == 14 then
                     hasA = true
                     break
                 end
             end
-
             if has7 and hasA then
                 card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_mod
                 return {
@@ -39,9 +33,9 @@ SMODS.Joker{
                 mult = card.ability.extra.mult
             }
         end
-	end,
-	set_badges = function (self, card, badges)
-		badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
-		badges[#badges+1] = create_badge(localize('k_roff_credit_l6_code'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
-	end
+    end,
+    set_badges = function (self, card, badges)
+        badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
+        badges[#badges+1] = create_badge(localize('k_roff_credit_l6_code'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
+    end
 }
