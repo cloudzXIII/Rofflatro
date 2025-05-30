103f5ba7148fc28a1fed1f61f9f49a046b0479b2
threeofaband <sycisytation@gmail.com>
2025-05-31 00:32:44 +0800

    Update Engagement.lua

--- cards/jokers/Engagement.lua
+++ cards/jokers/Engagement.lua
@@ -4,18 +4,7 @@ SMODS.Joker {
    rarity = 2,
    cost = 6,
    atlas = 'roffers',
-   pos = { x = 2, y = 7 },
-   loc_txt = {
-      name = 'Engagement',
-      text = {
-         'Creates a {C:spectral}Spectral{}',
-         'if no hands, discards,',
-         'or consumables left',
-         'at end of round',
-         '{C:inactive,s:0.8}Make sure to like and comment!',
-         '{C:inactive,s:0.6}(or maybe even be trans   parent)',
-      }
-   },
+   pos = { x = 2, y = 8 },
    calculate = function(self, card, context)
       if context.end_of_round and context.cardarea == G.jokers and (#G.consumeables.cards + G.GAME.consumeable_buffer) < G.consumeables.config.card_limit and G.consumeables and #G.consumeables.cards == 0 and G.GAME.current_round.hands_left == 0 and G.GAME.current_round.discards_left == 0 then
          G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
