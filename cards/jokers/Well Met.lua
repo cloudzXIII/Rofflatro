b27e9b426841faf61824b4bae2c7b441f37ccb9d
threeofaband <sycisytation@gmail.com>
2025-05-31 22:28:31 +0800

    Update Well Met.lua

--- cards/jokers/Well Met.lua
+++ cards/jokers/Well Met.lua
@@ -1,9 +1,9 @@
 SMODS.Joker{
    key = 'wellmet',
    atlas = 'roffers',
-	blueprint_compat = true,
-	eternal_compat = true,
-	pos = { x = 4, y = 2 }, --change if needed
+   blueprint_compat = true,
+   eternal_compat = true,
+   pos = { x = 4, y = 2 }, --change if needed
    rarity = 2,
    cost = 6,
    config = {
@@ -36,39 +36,31 @@ SMODS.Joker{
       }
    end,
    calculate = function(self, card, context)
-      if context.individual and not context.end_of_round and context.cardarea == G.hand then
-         if context.other_card.ability.roff_favorite then
-            if context.other_card.debuff then
-               return {
-                  message = localize('k_debuffed'),
-                  colour = G.C.RED,
-                  card = context.other_card,
-               } 
-            else
-               if card.ability.extra.check == nil or card.ability.extra.check == context.other_card then -- reference the first scored favorite card so retrigger effects are possible
-                  card.ability.extra.check = context.other_card
-                  return {
-                     x_mult = card.ability.extra.xmult,
-                     card = context.other_card,
-                     message = 'Well Met!'
-                  }
-               end
-            end
+      if context.individual and not context.end_of_round and context.cardarea == G.hand and context.other_card.ability.roff_favorite then
+         if context.other_card.debuff then
+            return {
+               message = localize('k_debuffed'),
+               colour = G.C.RED,
+               card = context.other_card,
+            }
+         else
+            return {
+               x_mult = card.ability.extra.xmult,
+               card = context.other_card,
+               message = 'Well Met!'
+            }
          end
-      end
-      if context.after then
-      	 card.ability.extra.check = nil
       end
    end,
    update = function(self, card, dt)
-       card.ability.extra.marqueetimer = card.ability.extra.marqueetimer + G.real_dt
-       if card.ability.extra.marqueetimer > 0.2 then
-           card.ability.extra.marqueetimer = 0
-           card.ability.extra.flavortext = ROFF.funcs.marquee(card.ability.extra.flavortext)
-       end
+      card.ability.extra.marqueetimer = card.ability.extra.marqueetimer + G.real_dt
+      if card.ability.extra.marqueetimer > 0.2 then
+         card.ability.extra.marqueetimer = 0
+         card.ability.extra.flavortext = ROFF.funcs.marquee(card.ability.extra.flavortext)
+      end
    end,
-	set_badges = function (self, card, badges)
-		badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
-		badges[#badges+1] = create_badge(localize('k_roff_credit_canicao_code'), ROFF.C.credits.canicao, ROFF.C.credits.canicao_text, 0.8)
-	end
+   set_badges = function (self, card, badges)
+      badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
+      badges[#badges+1] = create_badge(localize('k_roff_credit_canicao_code'), ROFF.C.credits.canicao, ROFF.C.credits.canicao_text, 0.8)
+   end
 }
