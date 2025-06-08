7c72bdfa7ffc15cf1ce69c9c52bbf496bc342345
threeofaband <sycisytation@gmail.com>
2025-06-09 01:22:18 +0800

    Add files via upload

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -90,6 +90,18 @@ return {
                     "{C:inactive,s:0.8,E:2}Ew! Why won't you {C:inactive,s:0.8,E:1}DIE?!"
                 }
             },
+            j_roff_excommunication = {
+                name = 'Excommunication',
+                text = {
+                'Held {C:spectral}Spectrals{} turn {C:dark_edition,E:1}Negative',
+                'This Joker gains {X:mult,C:white}X#1#{} Mult',
+                'when a {C:spectral}Spectral{} card is {C:attention}used{}',
+                'and {X:mult,C:white}X#2#{} Mult if {C:attention}sold{} instead',
+                '{C:inactive}(Currently {X:mult,C:white}X#3#{} {C:inactive}Mult)',
+                '{C:inactive,s:0.8,E:2}I ain\'t afraid of no... Ghost Deck?',
+                '{C:inactive,s:0.6,E:2}that\'s a lie from our vanilla balancer',
+            }
+         },
             j_roff_dvd = {
                 name = 'DVD',
                 text = {
@@ -215,12 +227,12 @@ return {
             },
             j_roff_thumbnail = {
                 name = "The Thumbnail",
-				text = {
-				    '{X:mult,C:white}X#1#{} Mult if exactly',
-					'{C:attention}#2#{} Jokers are owned',
-					'{C:inactive}(The Thumbnail included){}',
-					"{C:inactive,s:0.8,E:2}It's always clickbait!{}"
-				}
+                    text = {
+                        '{X:mult,C:white}X#1#{} Mult if exactly',
+                         '{C:attention}#2#{} Jokers are owned',
+                         '{C:inactive}(The Thumbnail included){}',
+                         "{C:inactive,s:0.8,E:2}It's always clickbait!{}"
+                    }
             },
             j_roff_crashout = {
                 name = "Crashout",
@@ -230,7 +242,7 @@ return {
                     "This Joker gains {X:mult,C:white}X#2#{} Mult",
                     "per consumable destroyed",
                     "{C:inactive}(Currently {X:mult,C:white}X#1#{} {C:inactive}Mult)",
-					"{C:inactive,s:0.8,E:2}Why is streamer mad?{}"
+                         "{C:inactive,s:0.8,E:2}Why is streamer mad?{}"
                 }
             },
             j_roff_neverskip = {
@@ -292,8 +304,106 @@ return {
                     "{S:1.1,C:red,E:2}Self destructs{} after triggering",
                     '{C:inactive}(Cannot scale Main Channel){}',
                     '{C:inactive,s:0.8,E:2}Wait, he has a main channel?'
-                }	
-            }
+                }     
+            },
+            j_roff_getchips = {
+                name = 'Just Get Chips',
+                text = {
+                    'This Joker copies',
+                    'all {C:chips}chip{} effects',
+                    'of all owned {C:attention}Jokers',
+                    "{C:inactive,s:0.8,E:2}We're on Plasma Deck, right?..."
+                }
+            },
+            j_roff_moralsupport = {
+                name = 'Buddy',
+                text = {
+                    '{C:dark_edition}+1{} Joker slot',
+                    "{C:inactive,s:0.8,E:2}Just here for the vibes."
+                }
+            },
+            j_roff_catfood = {
+                name = 'Cat Food',
+                text = {
+                    "Retriggers {C:attention}Lucky{} cards",
+                    "{C:attention}once{} if both effects",
+                    "{C:mult}fail{} to trigger",
+                    "{C:inactive,s:0.8,E:2}My cat's starving, please feed it!"
+                }
+            },
+             j_roff_cat_ears = {
+                name = 'Cat Ears',
+                text = {
+                    "This Joker gains {C:chips}+#1#{} Chips if",
+                    "played hand contains at least",
+                    "{C:attention}2{} unique scoring cards",
+                    "{C:inactive}(i.e. different rank or suit){}",
+                    "{C:inactive}(Currently {C:chips}+#2#{}{C:inactive} Chips){}",
+                    "{C:inactive,s:0.8,E:2}Do it, streamer. Put them on."
+                }
+            },
+            j_roff_free_negatives = {
+                name = 'Free Negatives',
+                text = {
+                    "For the next {C:attention}#1#{} round(s),",
+                    "gain a {C:dark_edition}Negative Tag{}",
+                    "at end of shop",
+                    "{C:inactive,s:0.8,E:2}More Jokers, more score!"
+                }
+            },
+            j_roff_engagement = {
+                name = 'Engagement',
+                text = {
+                    'Creates a {C:spectral}Spectral{} card',
+                    'if no {C:chips}hands{}, {C:mult}discards{},',
+                    'or {C:attention}consumables{} left',
+                    'at end of round',
+                    "{C:inactive}(Must have room)",
+                    '{C:inactive,s:0.8,E:2}Make sure to like and comment!',
+                    '{C:inactive,s:0.6,E:2}(or maybe even be trans   parent)',
+                }
+            },
+            j_roff_faces = {
+                name = 'Where Faces?',
+                text = {
+                    'Discarded {C:attention}face{} cards',
+                    'have a {C:green}#1# in #2#{} chance',
+                    'to be {C:attention}destroyed{}',
+                    'and give {C:money}#3#${}',
+                    "{C:inactive,s:0.8,E:2}Who killed my face cards?!"
+                }
+            },
+            j_roff_nice = {
+                name = 'Nice',
+                text = {
+                    'Scoring {C:attention}6s{} give {V:1}#1#{} #3#',
+                    'Held in hand {C:attention}9s{} give {V:2}#2#{} #4#',
+                    'Switches when hand played',
+                    '{C:inactive,s:0.8,E:2}Nice.'
+                }
+            },
+            j_roff_clam = {
+                name = 'Always Clam!',
+                text = {
+                    'Gains {C:chips}+#1#{} Chips if hand',
+                    'scores less than {C:attention}25% of',
+                    'score requirement, resets',
+                    'when {C:attention}Boss Blind{} is defeated',
+                    '{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)',
+                    '{C:inactive,s:0.8,E:2}Never been mad in my life!'
+                }
+            },
+            j_roff_vod_watchers = {
+                name = 'VOD Watchers',
+                text = {
+                    'Gains {X:mult,C:white}X#1#{} Mult',
+                    'per {C:attention}hand played',
+                    'Resets when beating',
+                    'a Blind in {C:attention}one hand',
+                    '{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Chips)',
+                    '{C:inactive,s:0.8,E:2}Don\'t wake them up!'
+                }
+            },
         },
     Back = 
     {
@@ -316,7 +426,7 @@ return {
                 "{s:1.5,C:attention,E:2}Credits:",
                 "{C:dark_edition,E:2}Lucky6(AlrexX){} - Lead, Art, Code",
                 "{C:dark_edition,E:2}Maxx{} - Design, Art, Code",
-                "{C:dark_edition,E:2}canicao{} - Design, Code",
+                "{C:dark_edition,E:2}canicao{} - Design, Art, Code",
                 "{C:dark_edition,E:2}garb{} - Code (Streamer Deck)",
                 "{C:dark_edition,E:2}UHadMeAtFood{} - Art, Code",
                 " ",
@@ -334,6 +444,7 @@ return {
             c_roff_kali = 'Trigger Happy',
             c_roff_a7x = 'Where Are My Sevens?',
             c_roff_nofaves = 'No Favorites',
+            c_roff_fashion = 'Fashion Disaster',
         },
         v_text = {
             ch_c_kali_spawn = {"Creates a Perishable {C:attention}Kali's Banhammer{} when boss blind selected"},
@@ -351,11 +462,19 @@ return {
             k_roff_doc_upgrade = "Muhhhhnee!",
             k_roff_doc_reset = "Bummer...",
             k_roff_mainchannel_upgrade = "What?!",
+            k_roff_negatives_trigger = "Yippee!",
+
+            k_roff_buddy_msg1 = "You got this!",
+            k_roff_buddy_msg2 = "I believe!",
+            k_roff_buddy_msg3 = "Go gettem!",
+            k_roff_buddy_msg4 = "To the moon!",
+            k_roff_buddy_msg5 = "You're the best!",
 
             k_roff_credit_l6_art = 'Art: Lucky6/AlrexX',
             k_roff_credit_l6_code = 'Code: Lucky6/AlrexX',
             k_roff_credit_maxx_art = 'Art: Maxx',
             k_roff_credit_maxx_code = 'Code: Maxx',
+            k_roff_credit_canicao_art = 'Art: canicao',
             k_roff_credit_canicao_code = 'Code: canicao',
             k_roff_credit_garb_code = 'Code: garb',
             k_roff_credit_uhadme_art = 'Art: UHadMeAtFood',
