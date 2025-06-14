85838a5c06189d29247056ff52d7edcc10571c4d
threeofaband <sycisytation@gmail.com>
2025-06-14 15:33:15 +0800

    Update Voucher Clear.lua

--- cards/jokers/Voucher Clear.lua
+++ cards/jokers/Voucher Clear.lua
@@ -1,47 +1,38 @@
 SMODS.Joker{
-	key = 'voucherclear',
-	atlas = 'roffers',
-	rarity = 2,
-	blueprint_compat = true,
-	eternal_compat = true,
-	pos = { x = 1, y = 5 },
-	config = { extra = {
-		pool_index = 0
-		}
-	},
-	cost = 4,
-	loc_vars = function(self,info_queue,card)
-		return {vars = {}}
-	end,
-	calculate = function(self,card,context)
-
-		if context.selling_self and G.shop_vouchers then
-
-			for i = 1, #G.shop_vouchers.cards do
-					-- This part destroys the G.shop_vouchers.cards[i].
-					play_sound('tarot1')
-
-					G.E_MANAGER:add_event(Event({
-						trigger = 'after',
-						delay = 0.3,
-						blockable = false,
-						func = function()
-							SMODS.remove_pool(G.P_CENTER_POOLS.Voucher, G.shop_vouchers.cards[i].config.center_key)
-							G.shop_vouchers.cards[i]:start_dissolve({HEX("57ecab")}, nil, 1.6)
-							if G.GAME.current_round.voucher.spawn[G.shop_vouchers.cards[i].config.center_key] then
-								G.GAME.current_round.voucher.spawn[G.shop_vouchers.cards[i].config.center_key] = false
-							end
-							G.jokers:remove_card(G.shop_vouchers.cards[i])
-							--G.shop_vouchers.cards[i]:remove()
-							G.shop_vouchers.cards[i] = nil
-							return true;
-						end
-					}))
-				end
-			end
-		end,
-	set_badges = function (self, card, badges)
-		badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
-		badges[#badges+1] = create_badge(localize('k_roff_credit_uhadme_code'), ROFF.C.credits.uhadme, G.C.WHITE, 0.8)
-	end
+    key = 'voucherclear',
+    atlas = 'roffers',
+    rarity = 2,
+    blueprint_compat = true,
+    eternal_compat = true,
+    pos = { x = 1, y = 5 },
+    config = { extra = { last_blind = 'Small'} },
+    cost = 4,
+    calculate = function(self,card,context)
+        if  G.GAME.round_resets.blind_states.Big == 'Defeated' and G.GAME.round_resets.blind_states.Boss  == 'Upcoming' and context.ending_shop and card.ability.extra.last_blind == "Big" and G.shop_vouchers then
+            for i = 1, #G.shop_vouchers.cards do
+                -- This part destroys the G.shop_vouchers.cards[i].
+                play_sound('slice1')
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'before',
+                    delay = 0.3,
+                    blockable = false,
+                    func = function()
+                        SMODS.remove_pool(G.P_CENTER_POOLS.Voucher, G.shop_vouchers.cards[i].config.center_key)
+                        G.shop_vouchers.cards[i]:start_dissolve({HEX("57ecab")}, nil, 1.6)
+                        if G.GAME.current_round.voucher.spawn[G.shop_vouchers.cards[i].config.center_key] then
+                            G.GAME.current_round.voucher.spawn[G.shop_vouchers.cards[i].config.center_key] = false
+                        end
+                        G.jokers:remove_card(G.shop_vouchers.cards[i])
+                        --G.shop_vouchers.cards[i]:remove()
+                        G.shop_vouchers.cards[i] = nil
+                        return true;
+                    end
+                }))
+            end
+        end
+    end,
+    set_badges = function (self, card, badges)
+        badges[#badges+1] = create_badge(localize('k_roff_credit_l6_art'), ROFF.C.credits.Lucky6, G.C.WHITE, 0.8)
+        badges[#badges+1] = create_badge(localize('k_roff_credit_uhadme_code'), ROFF.C.credits.uhadme, G.C.WHITE, 0.8)
+    end
 }
