7fe1a4c1fa6db02f2c56a3e254a9b446287d9bb8
threeofaband <sycisytation@gmail.com>
2025-07-06 16:29:07 +0800

    Update Streamer Deck.lua

--- cards/Streamer Deck.lua
+++ cards/Streamer Deck.lua
@@ -9,3 +9,15 @@ SMODS.Back({
     end,
     unlock_condition = {type = 'win_deck', deck = 'b_red'}
 })
+
+SMODS.Back({
+    key = 'highscoring',
+    atlas = 'reffs',
+    pos = { x = 0, y = 0 },    
+    unlocked = true,
+    discovered = true,
+    config = {},
+    loc_vars = function(self, info_queue, card)
+    end,
+    unlock_condition = {type = 'win_deck', deck = 'b_red'}
+})
