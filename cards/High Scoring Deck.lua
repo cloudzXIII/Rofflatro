0cf81691129c30d7bdf56195db2f7bf99dc70131
threeofaband <sycisytation@gmail.com>
2025-07-06 17:58:50 +0800

    Update High Scoring Deck.lua

--- cards/High Scoring Deck.lua
+++ cards/High Scoring Deck.lua
@@ -4,8 +4,10 @@ SMODS.Back({
     pos = { x = 1, y = 0 },    
     unlocked = true,
     discovered = true,
-    config = {},
-    loc_vars = function(self, info_queue, card)
+    calculate = function(self, back, context)
+        if context.end_of_round and context.main_eval and G.GAME.round_resets.ante >= 2 then
+            SMODS.Mods.Roffle.config.highscoring.randomise = true
+        end
     end,
     unlock_condition = {type = 'win_deck', deck = 'b_red'}
 })
