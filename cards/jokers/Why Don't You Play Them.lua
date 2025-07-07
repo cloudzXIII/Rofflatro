0c2910e10ef5a93757009181b8babaaeb235fd2e
threeofaband <sycisytation@gmail.com>
2025-07-07 17:38:49 +0800

    5 mult to 2 mult

--- cards/jokers/Why Don't You Play Them.lua
+++ cards/jokers/Why Don't You Play Them.lua
@@ -5,7 +5,7 @@ SMODS.Joker {
    rarity = 3,
    cost = 10,
    blueprint_compat = true,
-   config = { extra = { mult = 5 } },
+   config = { extra = { mult = 2 } },
    loc_vars = function(self, info_queue, card)
       info_queue[#info_queue+1] = {key = 'roff_favorite', set = 'Other'}
       return { vars = { card.ability.extra.mult } }
